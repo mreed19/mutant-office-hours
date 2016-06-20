4cc89c2eb9941e4babb024ec13032f769d4a594c
mreed19 <michael.reed315@gmail.com>
2016-06-20 15:14:18 -0400

    Adds basic mutantList controller setup. Adds controller and controllerAs to module config. Adds a simple binding to mutantList.html to show controller is functioning

---
+++ app/mutantList/mutantList.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular.module('mutantApp.mutantList')
+    .controller('MutantListController', MutantListController);
+
+  function MutantListController() {
+    var vm = this;
+    vm.mutants = ['deadpool', 'nightcrawler', 'gambit'];
+  }
+})();
