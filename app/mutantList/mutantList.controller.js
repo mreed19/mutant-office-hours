4e5c1e6fd409deba047aac43d5aef7b96a1f85d2
mreed19 <michael.reed315@gmail.com>
2016-06-20 15:37:35 -0400

    Add ng-submit to the form. Add ng-model to all form fields. Add mutant constructor to the controller.

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -1,11 +1,27 @@
 (function() {
   'use strict';
 
-  angular.module('mutantApp.mutantList')
+  angular
+    .module('mutantApp.mutantList')
     .controller('MutantListController', MutantListController);
 
   function MutantListController() {
     var vm = this;
+
+    vm.addMutant = addMutant;
     vm.mutants = ['deadpool', 'nightcrawler', 'gambit'];
+    vm.newMutant = new Mutant();
+
+    function Mutant() {
+      this.name = '';
+      this.phone = '';
+      this.topic = '';
+      this.notified = false;
+      this.complete = false;
+    }
+
+    function addMutant() {
+      vm.mutants.push(vm.newMutant);
+    }
   }
 })();
