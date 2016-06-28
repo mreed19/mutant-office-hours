92e7c0c6f45b323917ab4ce9ff267e9608f93a0d
mreed19 <michael.reed315@gmail.com>
2016-06-28 14:44:15 -0400

    Add MutantFormController to directive. Add isolate scope and pass vm.mutants into it through mutants property. Add addMutant and newMutant to controller. Remove unnecessary functions from MutantListController

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -9,17 +9,10 @@
   function MutantListController(mutantService, textMessageService, user) {
     var vm = this;
 
-    vm.addMutant = addMutant;
     vm.deleteMutant = deleteMutant;
     vm.toggleComplete = toggleComplete;
     vm.sendText = sendText;
     vm.mutants = mutantService.mutantsByUser(user.uid);
-    vm.newMutant = new mutantService.Mutant();
-
-    function addMutant() {
-      vm.mutants.$add(vm.newMutant);
-      vm.newMutant = new mutantService.Mutant();
-    }
 
     function deleteMutant(mutant) {
       vm.mutants.$remove(mutant);
